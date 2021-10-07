3b748f57b33d02731fe40f93dc822f28d1a9d9d3
Rodrigo Pauletti <digopauletti@yahoo.com.br>
2021-10-07 18:52:04 -0300

    Fix "Access-Control-Allow-Headers" value

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(function (req, res, next) {
   // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
 
   // Set to true if you need the website to include cookies in the requests sent
