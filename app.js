e37c6ebc1db72802e0e7875430718fe3805ca77f
Rodrigo Pauletti <digopauletti@yahoo.com.br>
2022-05-18 20:19:52 -0300

    initial route test 10

--- app.js
+++ app.js
@@ -60,10 +60,16 @@ mongoose
 //   "preflightContinue": false,
 //   "optionsSuccessStatus": 204
 // }));
-app.use(cors({
-  origin: '*',
-  optionsSuccessStatus: 200
-}));
+// app.use(cors({
+//   origin: '*',
+//   optionsSuccessStatus: 200
+// }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // app.options('*', cors()) // include before other routes
 
@@ -81,7 +87,7 @@ if (process.env.NODE_ENV === "PROD") {
 }
 
 app.get('/', function (req, res) {
-  res.send('Hello World!9');
+  res.send('Hello World!10');
 });
 
 app.use("/files", express.static(path.resolve(__dirname, "tmp", "uploads")));
